a51eee160824719bb1d5985aee9a259ec786940c
Anoop Koganti <anoopkoganti05@gmail.com>
2026-04-25 18:06:16 -0700

    Initial regen code

---
+++ fsae-ccm/src/peripherals/regen.cpp
@@ -0,0 +1,9 @@
+#include "arduino_freertos.h"
+#include "peripherals/regen.h"
+#include "./vehicle/devices/bse.h"
+
+// Anteater Electric Racing, 2026
+
+float regen_GetRegenPercent(float ) {
+    regenData.brakePercent = LINEAR_MAP()
+}

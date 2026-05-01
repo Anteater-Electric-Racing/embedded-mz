6821b80561b50eba5ddfbfcbd0b00a3d001fc267
kdao18 <kennethdao18@gmail.com>
2026-05-01 15:25:55 -0700

    removed debug flag

--- fsae-ccm/src/vehicle/faults.cpp
+++ fsae-ccm/src/vehicle/faults.cpp
@@ -61,9 +61,9 @@ void Faults_SetFault(FaultType fault) {
         break;
     }
     case FAULT_CAN: {
-#if DEBUG_FLAG
+// #if DEBUG_FLAG
         Serial.println("Setting CAN Timeout Fault");
-#endif
+// #endif
         faultBitMap |= FAULT_CAN_MASK;
         break;
     }

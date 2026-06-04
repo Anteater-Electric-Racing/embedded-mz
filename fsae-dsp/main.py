bec0294b5c483d3c580916065b18a6042fdf66f8
Kang Ji <kangkuan9@gmail.com>
2026-06-04 18:24:38 +0100

    Luma driver working correctly now

--- fsae-dsp/main.py
+++ fsae-dsp/main.py
@@ -1,12 +1,12 @@
 from luma.core.interface.serial import spi
-from luma.core.render impotr canvas
+from luma.core.render import canvas
 from luma.oled.device import ssd1351
 from time import sleep
 
-serial = spi()
+serial = spi(device = 0, port = 0)
 
 device  = ssd1351(serial)
 
 with canvas(device) as draw:
     draw.text((30, 40), "please work", fill = "white")
-    sleep(1000)
+sleep(1000)

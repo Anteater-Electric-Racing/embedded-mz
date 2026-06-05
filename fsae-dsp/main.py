c428622ce8dce7bbfdbd121a45fe574f83d65ca9
Kang Ji <kangkuan9@gmail.com>
2026-06-05 07:46:59 +0100

    implmented ingest_mqtt

--- fsae-dsp/main.py
+++ fsae-dsp/main.py
@@ -3,7 +3,7 @@ from luma.core.render import canvas
 from luma.oled.device import ssd1351
 from time import sleep
 import paho.mqtt.subscribe as subscribe
-
+import json
 
 
 #Ingest data
@@ -15,30 +15,43 @@ import paho.mqtt.subscribe as subscribe
 RED = "RED"
 GREEN = "GREEN"
 SELECTED = "pack_current"
+TOPIC = "telemetry"
+HOST = "127.0.0.1"
+SUBSCRIPTION = ""
 
 def parse_args():
+    global SUBSCRIPTION
     #planned flags:
     # -h help
     # --select what column
     # --warn [none/warn/fault] look for faults or faults + warns or ignore faults and wanrs 
     print("parse_args stub")
+    SUBSCRIPTION = subscribe.simple(TOPIC, hostname=HOST)
+    #this function will also have to reintialize the subscriber but anyways
+
+def ingest_mqtt():
+   payload = json.loads(SUBSCRIPTION.payload)
+   return payload
 
 
-int main:
+def main():
     parse_args()
     
     while True: 
-        mqtt_msg = ingest_mqtt();
-        if (has_warning(mqtt_msg)):
+        mqtt_msg = ingest_mqtt()
+        """if (has_warning(mqtt_msg)):
             warnings = extract_warnings(mqtt_msg)
             for warn in warning:
                 display(warn, RED, delay = 100)
-        
-        selected_data = extract_column(mqtt_msg, GREEN) 
-
+        else:
+            selected_data = extract_column(mqtt_msg, GREEN) 
+"""
 #serial = spi(device = 0, port = 0)
 
 #device  = ssd1351(serial)
 #with canvas(device) as draw:
 #    draw.text((30, 40), "please work", fill = "white")
 #sleep(1000)
+
+if __name__ == "__main__":
+    main()

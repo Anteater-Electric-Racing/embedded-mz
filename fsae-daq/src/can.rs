fa3f181da52a9b95f184d2c3b657a4d32f7ba1eb
Kang Thinkpad <kangkuan9@gmail.com>
2026-07-03 19:01:34 -0700

    started new can packet format

--- fsae-daq/src/can.rs
+++ fsae-daq/src/can.rs
@@ -255,6 +255,110 @@ impl Reading for TelemetryData {
     }
 }
 
+#[derive(
+    Serialize, Deserialize, Default, Debug, Clone, PartialEq, DekuRead, DekuWrite, DekuSize,
+)]
+#[deku(endian = "little")]pub struct TelemetryData2 {
+    pub apps_travel: f32,
+
+    pub bse_front: f32,
+    pub bse_rear: f32,
+    pub bse_avg: f32,
+
+    pub imd_resistance: f32,
+    pub imd_status: u32,
+
+    pub pack_voltage: f32,
+    pub pack_current: f32,
+    pub soc: f32,
+    pub discharge_limit: f32,
+    pub charge_limit: f32,
+    pub low_cell_volt: f32,
+    pub high_cell_volt: f32,
+    pub avg_cell_volt: f32,
+
+    //MCU data
+    pub control_mode: u8,
+    pub target_iq: f32,
+
+    pub motor_position: f32,
+    pub is_motor_still: u8,
+    pub e_rpm: f32,
+    pub duty_cycle: f32,
+    pub input_voltage: f32,
+    pub ac_current: f32,
+    pub dc_current: f32,
+    pub controller_temp: f32,
+    pub motor_temp: f32,
+    pub fault_code: u8,
+    pub foc_id: f32,
+    pub foc_iq: f32,
+    
+    pub drive_enabled: u8,
+
+    pub max_ac_current: f32,
+    pub av_max_ac_current: f32,
+    pub min_ac_current: f32,
+    pub av_min_ac_current: f32,
+    pub max_dc_current: f32,
+    pub av_max_dc_current: f32,
+    pub min_dc_current: f32,
+    pub av_min_dc_current: f32,
+
+    pub dti_throttle_input: f32,
+    pub dti_brake_input: f32,
+
+    pub digital_in1: bool,
+    pub digital_in2: bool,
+    pub digital_in3: bool,
+    pub digital_in4: bool,
+    pub digital_out1: bool,
+    pub digital_out2: bool,
+    pub digital_out3: bool,
+    pub digital_out4: bool,
+
+    // Limits Active - Group 1
+    pub cap_temp_limit_active: bool,
+    pub dc_temp_limit_active: bool,
+    pub drive_enable_limit_active: bool,
+    pub igbt_accel_limit_active: bool,
+    pub igbt_temp_limit_active: bool,
+    pub input_voltage_limit_active: bool,
+    pub motor_accel_temp_limit_active: bool,
+    pub motor_temp_limit_active: bool,
+
+    // Limits Active - Group 2
+    pub rpm_min_limit_active: bool,
+    pub rpm_max_limit_active: bool,
+    pub power_limit_active: bool,
+    
+    pub can_namp_version: u8,
+
+    #[deku(bits = 1)]
+    pub osr_current: bool,
+    #[deku(bits = 1)]
+    pub under_voltage: bool,
+    #[deku(bits = 1)]
+    pub over_temperature: bool,
+    #[deku(bits = 1)]
+    pub apps: bool,
+    #[deku(bits = 1)]
+    pub bse: bool,
+    #[deku(bits = 1)]
+    pub bpps: bool,
+    #[deku(bits = 1)]
+    pub apps_brake_plaus: bool,
+    #[deku(bits = 1, pad_bits_after = "24")]
+    pub low_battery_voltage: bool,
+}
+
+impl Reading for TelemetryData2 {
+    fn topic() -> &'static str {
+        "telemetry"
+    }
+}
+
+
 //I'm very sorry but I honestly can't figure out how to properly have a global & mutable buffer (which rust kinda doesn't want to have (no shared mutable states)), so I;m doing some architectural ersosion and putting buffer here. 
 
 /// Reads ISO-TP packets from `can0` in a loop, parses each into

2396ee64d665db84c1587620b24b9b2bed39ef1d
Anoop Koganti <anoopkoganti05@gmail.com>
2026-09-05 13:58:54 -0700

    bruh

--- fsae-ccm/src/utils/utils.h
+++ fsae-ccm/src/utils/utils.h
@@ -36,8 +36,8 @@ constexpr int fault_address = 0;
 #define WSS_FLAG 0
 #define BMS_FLAG 0 // REMOVE
 #define IMD_FLAG 0
-#define APPS_DEBUG 1
-#define BSE_DEBUG 1
+#define APPS_DEBUG 0
+#define BSE_DEBUG 0
 #define PRECHARGE_DEBUG 0
 #define LP_FLAG 0
 

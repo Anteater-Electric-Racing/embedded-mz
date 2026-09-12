9752a7bf90ba845e0a5296fa67d53f14cc77d909
Anoop Koganti <anoop.koganti@redwoodmaterials.com>
2026-09-11 22:02:21 -0700

    testing

--- fsae-pcc/src/main.cpp
+++ fsae-pcc/src/main.cpp
@@ -81,9 +81,9 @@ void threadMain(void *pvParameters) {
         Serial.print(digitalRead(IR_PLUS));
         Serial.print(" | IR -: ");
         Serial.print(digitalRead(IR_MINUS));
-        Serial.print(" | Precharge Time: ");
-        Serial.print(times_s.PreChargeTime);
-        Serial.print(" ms");
+        // Serial.print(" | Precharge Time: ");
+        // Serial.print(times_s.PreChargeTime);
+        // Serial.print(" ms");
         Serial.print("\r");
         vTaskDelay(100);
     }
